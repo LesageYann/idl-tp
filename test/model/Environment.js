2e31e888675285b321cbfa39aaf8fd1cf5b83b14
valtena <ethanvaltena@gmail.com>
2017-01-13 18:30:07 +0100

    refactor test

--- test/model/Environment.js
+++ test/model/Environment.js
@@ -1,36 +1,34 @@
 var expect = chai.expect;
 
-describe("Environment", function () {
-  describe("constructor", function () {
-    it("should be not initialized state", function () {
-      expect(Environment.initialized).to.not.equal(true);
-    });
+describe( "Environment", function () {
+  describe( "constructor", function () {
+    it( "should be not initialized state", function () {
+      expect( Environment.initialized ).to.not.equal( true );
+    } );
 
-    it("should pass at initialized state", function () {
-      var env = new Environment(50,50,true);
-      expect(Environment.initialized).to.equal(true);
-    });
+    it( "should set the number of lines", function () {
+      var env = new Environment( 50, 50, true );
+      expect( env._x ).to.equal( 50 );
+    } );
 
-    it("should set the number of lines", function () {
-      var env = new Environment(50,50,true);
-      expect(env._x).to.equal(50);
-    });
+    it( "should set the number of colums", function () {
+      var env = new Environment( 50, 50, true );
+      expect( env._y ).to.equal( 50 );
+    } );
+  } );
 
-    it("should set the number of colums", function () {
-      var env = new Environment(50,50,true);
-      expect(env._y).to.equal(50);
-    });
-  });
+  describe( "#fonctions", function () {
+    describe( "#isFree", function () {
+      it( "isFree on free case return true", function () {
+        var env = new Environment( 50, 50, true );
+        expect( env.isFree( {
+          x: 20,
+          y: 20
+        } ) ).to.equal( true );
+      } );
 
-  describe("#fonctions", function () {
-    describe("#isFree", function () {
-      it("isFree on free case return true", function () {
-        var env = new Environment(50,50,true);
-        expect(env.isFree(20, 20)).to.equal(true);
-      });
-
-      it("isFree on occuped case return false", function () {
-        var env = new Environment(50,50,true);
+      it( "isFree on occuped case return false", function () {
+        var env = new Environment( 50, 50, true );
         var mockAgent = {
           x: function () {
             return 10;
@@ -39,14 +37,20 @@ describe("Environment", function () {
             return 10;
           }
         };
-        env.moveAgent(mockAgent, {x: 20, y: 20});
-        expect(env.isFree(20, 20)).to.equal(false);
-      });
-    });
-    
-    describe("#moveAgent", function () {
-      it("place the Agent at the good place", function () {
-        var env = new Environment(50,50,true);
+        env.moveAgent( mockAgent, {
+          x: 20,
+          y: 20
+        } );
+        expect( env.isFree( {
+          x: 20,
+          y: 20
+        } ) ).to.equal( false );
+      } );
+    } );
+
+    describe( "#moveAgent", function () {
+      it( "place the Agent at the good place", function () {
+        var env = new Environment( 50, 50, true );
         var mockAgent = {
           x: function () {
             return 10;
@@ -55,12 +59,18 @@ describe("Environment", function () {
             return 10;
           }
         };
-        env.moveAgent(mockAgent, {x: 20, y: 20});
-        expect(env.case(20, 20)).to.equal(mockAgent);
-      });
+        env.moveAgent( mockAgent, {
+          x: 20,
+          y: 20
+        } );
+        expect( env.case( {
+          x: 20,
+          y: 20
+        } ) ).to.equal( mockAgent );
+      } );
 
-      it("free the previous place", function () {
-        var env = new Environment(50,50,true);
+      it( "free the previous place", function () {
+        var env = new Environment( 50, 50, true );
         mockAgent = {
           x: function () {
             return 10
@@ -69,14 +79,26 @@ describe("Environment", function () {
             return 10
           }
         };
-        env.moveAgent(mockAgent, {x: 10, y: 10});
-        expect(env.isFree(10, 10)).to.equal(false);
-        env.moveAgent(mockAgent, {x: 20, y: 20});
-        expect(env.isFree(10, 10)).to.equal(true);
-      });
-      
-      it("return the agent A on X,Y if X,Y is not free when we move the agent B on X,Y", function () {
-        var env = new Environment(50,50,true);
+        env.moveAgent( mockAgent, {
+          x: 10,
+          y: 10
+        } );
+        expect( env.isFree( {
+          x: 10,
+          y: 10
+        } ) ).to.equal( false );
+        env.moveAgent( mockAgent, {
+          x: 20,
+          y: 20
+        } );
+        expect( env.isFree( {
+          x: 10,
+          y: 10
+        } ) ).to.equal( true );
+      } );
+
+      it( "return the agent A on X,Y if X,Y is not free when we move the agent B on X,Y", function () {
+        var env = new Environment( 50, 50, true );
         mockAgentA = {
           x: function () {
             return 10
@@ -93,11 +115,17 @@ describe("Environment", function () {
             return 10
           }
         };
-        env.moveAgent(mockAgentA, {x: 20, y: 20});
-        res = env.moveAgent(mockAgentB, {x: 20, y: 20});
-        expect(res).to.equal(mockAgentA);
-      });
-      
-    });
-  });
-});
+        env.moveAgent( mockAgentA, {
+          x: 20,
+          y: 20
+        } );
+        res = env.moveAgent( mockAgentB, {
+          x: 20,
+          y: 20
+        } );
+        expect( res ).to.equal( mockAgentA );
+      } );
+
+    } );
+  } );
+} );
