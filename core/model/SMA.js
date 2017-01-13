8180833eec593d2adb1f95d21e0ed13a8e0b7d3a
valtena <ethanvaltena@gmail.com>
2017-01-13 18:05:41 +0100

    beautify text with atom

--- core/model/SMA.js
+++ core/model/SMA.js
@@ -1,94 +1,94 @@
 class SMA {
-  constructor(agents){
+  constructor( agents ) {
     this._agents = agents;
     this._observers = [];
     this._hasChanged = false;
     this._tick = 0;
   }
 
-  run  () {
-        self = this;
-        self._tick = 1;
-        self._inTurn = false;
-        if (config.delay != 0) {
-          self._intervalID = window.setInterval(function () {
-            self.launchTurn(self);
-          }, config.delay);
-        }
-    };
+  run() {
+    self = this;
+    self._tick = 1;
+    self._inTurn = false;
+    if ( config.delay != 0 ) {
+      self._intervalID = window.setInterval( function () {
+        self.launchTurn( self );
+      }, config.delay );
+    }
+  };
 
-    launchTurn  () {
-      self =this;
-      if (!self._inTurn) {
-        self._inTurn = true;
-        self.turn();
-        if (self._tick == config.nbTicks) {
-          console.log("clear interval")
-          clearInterval(self._intervalID);
-        }
-        self._tick++;
-        self._inTurn = false;
+  launchTurn() {
+    self = this;
+    if ( !self._inTurn ) {
+      self._inTurn = true;
+      self.turn();
+      if ( self._tick == config.nbTicks ) {
+        console.log( "clear interval" )
+        clearInterval( self._intervalID );
       }
-    };
+      self._tick++;
+      self._inTurn = false;
+    }
+  };
 
-    turn  () {
-      this._executeTurn[config.sheduling](this);
-      
-      if (this.hasChanged() ) {
-        this.notifyObserver();
-      }
-    };
+  turn() {
+    this._executeTurn[ config.sheduling ]( this );
 
-    hasChanged  () {
-      return this._hasChanged;
-    };
+    if ( this.hasChanged() ) {
+      this.notifyObserver();
+    }
+  };
 
-    setChanged  () {
-      this._hasChanged = true;
-    };
+  hasChanged() {
+    return this._hasChanged;
+  };
 
-    addObserver  (observer) {
-      this._observers.push(observer);
-    };
+  setChanged() {
+    this._hasChanged = true;
+  };
 
-    notifyObserver  () {
-      for (var i = 0; i < this._observers.length; i++) {
-        this._observers[i].update(this._agents);
-      }
-      this._hasChanged = false;
-    };
+  addObserver( observer ) {
+    this._observers.push( observer );
+  };
+
+  notifyObserver() {
+    for ( var i = 0; i < this._observers.length; i++ ) {
+      this._observers[ i ].update( this._agents );
+    }
+    this._hasChanged = false;
+  };
 
 }
 
-(function(){
-  function fair(sma) {
-      var j, x, i;
-      for (i = sma._agents.length; i; i--) {
-        j = Math.floor(Math.random() * i);
-        x = sma._agents[i - 1];
-        sma._agents[i - 1] = sma._agents[j];
-        sma._agents[j] = x;
-      }
-      sma._executeTurn.sequential(sma);
-    };
-    
-    function random(sma) {
-      var i, l=sma._agents.length;
-      for (i = l; i; i--) {
-        sma._agents[Math.floor(Math.random() * l)].decide();
-      }
-    };
-    
-    function sequential(sma){
-      //nothing todo, always the same order
-      for (i = 0; i < sma._agents.length; i++) {
-        sma._agents[i].decide();
-      }
+( function () {
+  function fair( sma ) {
+    var j, x, i;
+    for ( i = sma._agents.length; i; i-- ) {
+      j = Math.floor( Math.random() * i );
+      x = sma._agents[ i - 1 ];
+      sma._agents[ i - 1 ] = sma._agents[ j ];
+      sma._agents[ j ] = x;
     }
+    sma._executeTurn.sequential( sma );
+  };
 
-    SMA.prototype._executeTurn = {
-      fair : fair,
-      random: random,
-      sequential: sequential
+  function random( sma ) {
+    var i, l = sma._agents.length;
+    for ( i = l; i; i-- ) {
+      sma._agents[ Math.floor( Math.random() * l ) ].decide();
     }
-}());
+  };
+
+  function sequential( sma ) {
+    //nothing todo, always the same order
+    for ( i = 0; i < sma._agents.length; i++ ) {
+      sma._agents[ i ].decide();
+    }
+  }
+
+  SMA.prototype._executeTurn = {
+    fair: fair,
+    random: random,
+    sequential: sequential
+  }
+}() );
