8af56b5578f4042984643276ffff7f6eb3eb3b91
valtena <ethanvaltena@gmail.com>
2017-01-14 17:36:58 +0100

    reset tableVue

--- core/vue/TableVue.js
+++ core/vue/TableVue.js
@@ -38,7 +38,6 @@ function TableVue( HTMLContainer, environment ) {
       this._style.innerHTML = style;
     };
 
-    }
 
     TableVue.prototype.init = function () {
       this._basicStyle = "table{border-collapse: collapse;width:" +
@@ -58,8 +57,6 @@ function TableVue( HTMLContainer, environment ) {
           tr.insertCell( x ).id = "x" + x + "y" + y;
         }
       }
-    }
-  };
 
       if ( config.canvasDisplay ) {
         if ( oldTable == null ) {
@@ -68,25 +65,11 @@ function TableVue( HTMLContainer, environment ) {
           this._container.replaceChild( this._canvas, oldTable );
         }
       }
-    }
-    this._style.innerHTML = style;
+    };
 
-    this._repaintInfos();
-  };
 
-  init() {
-    this._basicStyle = "table{border-collapse: collapse;width:" +
-      (config.grid.size.x * config.box.size) + config.box.unit +
-      "}#view{max-width:" + config.canvasSize.x + config.canvasSize.unit +
-      ";max-height:" + config.canvasSize.x + config.canvasSize.unit +
-      ";overflow: auto;}td{box-sizing: border-box;border:#f8f8f8 solid 1px;width: " + config.box.size +
-      config.box.unit + "; height: " + config.box.size + config.box.unit + "}";
-    this._style.innerHTML = this._basicStyle;
-    var oldTable = this._canvas;
-    this._canvas = document.createElement('table');
-    for (var y = 0; y < this._env.ySize(); y++) {
-      var tr = document.createElement('tr');
-      this._canvas.appendChild(tr);
+    TableVue.initialized = true;
+  }
 
   this.init();
 };
