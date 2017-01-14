57c94302dd8f998ce3e1878095eef62958e9904b
Marine Bal <MA@MacBook-Air-de-AnneKev.local>
2017-01-14 14:33:10 +0100

    fix some bug and refacto

--- core/model/Environment.js
+++ core/model/Environment.js
@@ -1,98 +1,104 @@
 class Environment {
-  constructor( x, y, toric ) {
-    this._x = x || 50;
-    this._y = y || 50;
-    this._toric = toric;
-    this._plan = [];
-    this._sma = {
-      setChanged: function () {}
-    }; //mock before set sma
-    for ( var i = 0; i < this._x; i++ ) {
-      this._plan[ i ] = [];
+    constructor(x, y, toric) {
+        this._x = x || 50;
+        this._y = y || 50;
+        this._toric = toric;
+        this._plan = [];
+        this._sma = {
+            setChanged: function () {
+            }
+        }; //mock before set sma
+        for (var i = 0; i < this._x; i++) {
+            this._plan[i] = [];
+        }
     }
-  }
 
-  isToric() {
-    return this._toric;
-  };
+    isToric() {
+        return this._toric;
+    };
 
-  xSize() {
-    return this._x;
-  };
+    xSize() {
+        return this._x;
+    };
 
-  ySize() {
-    return this._y;
-  };
+    ySize() {
+        return this._y;
+    };
 
-  setSMA( sma ) {
-    this._sma = sma;
-    //sma.addObserver(this);
-  };
+    setSMA(sma) {
+        this._sma = sma;
+        //sma.addObserver(this);
+    };
 
-  /* change position on plan
-   * return agent if the newPos is already occuped
-   */
-  moveAgent( agent, newPos ) {
-    this._handleBound( newPos );
-    this._plan[ agent.x() ][ agent.y() ] = null;
-    var res = this._plan[ newPos.x ][ newPos.y ];
-    this._plan[ newPos.x ][ newPos.y ] = agent;
-    this._sma.setChanged();
-    return res;
-  };
+    /* change position on plan
+     * return agent if the newPos is already occuped
+     */
+    moveAgent(agent, newPos) {
+        this._handleBound(newPos);
+        this._plan[agent.x()][agent.y()] = null;
+        var res = this._plan[newPos.x][newPos.y];
+        this._plan[newPos.x][newPos.y] = agent;
+        this._sma.setChanged();
+        return res;
+    };
 
-  /* change position on plan
-   * erase previous agent if the newPos is already occuped
-   */
-  setAgentAt( agent, newPos ) {
-    this._handleBound( newPos );
-    this._plan[ newPos.x ][ newPos.y ] = agent;
-    this._sma.setChanged();
-  };
+    /* change position on plan
+     * erase previous agent if the newPos is already occuped
+     */
+    setAgentAt(agent, newPos) {
+        this._handleBound(newPos);
+        this._plan[newPos.x][newPos.y] = agent;
+        this._sma.setChanged();
+    };
 
-  _handleBound( newPos ) {
-    if ( this._toric ) {
-      if ( newPos.x >= this._x || newPos.x < 0 ) {
-        newPos.x = ( newPos.x + this._x ) % this._x;
-      }
-      if ( newPos.y >= this._y || newPos.y < 0 ) {
-        newPos.y = ( newPos.y + this._y ) % this._y;
-      }
-    } else {
-      if ( newPos.x >= this._x || newPos.x < 0 ) {
-        throw new ExceptionXBound( newPos.x );
-      }
-      if ( newPos.y >= this._y || newPos.y < 0 ) {
-        throw new ExceptionYBound( newPos.y );
-      }
-    }
-  };
+    _handleBound(newPos) {
+        if (this._toric) {
+            if (newPos.x >= this._x || newPos.x < 0) {
+                newPos.x = ( newPos.x + this._x ) % this._x;
+            }
+            if (newPos.y >= this._y || newPos.y < 0) {
+                newPos.y = ( newPos.y + this._y ) % this._y;
+            }
+        } else {
+            if (newPos.x >= this._x || newPos.x < 0) {
+                throw new ExceptionXBound(newPos.x);
+            }
+            if (newPos.y >= this._y || newPos.y < 0) {
+                throw new ExceptionYBound(newPos.y);
+            }
+        }
+    };
 
-  aroundFree( pos ) {
-    varres = [];
-    for ( i = -1; i < 2; i++ ) {
-      this._addToFree( x + i, y + i, res );
-      this._addToFree( x + i, y + i + 1, res );
-    }
-    return res;
-  };
+    aroundFree(pos) {
+        varres = [];
+        for (i = -1; i < 2; i++) {
+            this._addToFree(x + i, y + i, res);
+            this._addToFree(x + i, y + i + 1, res);
+        }
+        return res;
+    };
 
-  _addToFree( x, y, arr ) {
-    if ( this.isFree( x, y ) ) {
-      arr.push( {
-        x: x,
-        y: y
-      } );
-    }
-  };
+    _addToFree(x, y, arr) {
+        console.log('add to free', x, y);
+        var pos = {
+            x: x,
+            y: y
+        };
+        if (this.isFree(pos)) {
+            arr.push({
+                x: x,
+                y: y
+            });
+        }
+    };
 
-case ( pos ) {
-  this._handleBound( pos );
-  return this._plan[ pos.x ][ pos.y ];
-};
+    case(pos) {
+        this._handleBound(pos);
+        return this._plan[pos.x][pos.y];
+    };
 
-isFree( pos ) {
-  return this._plan[ pos.x ][ pos.y ] == null;
-};
+    isFree(pos) {
+        return this._plan[pos.x][pos.y] == null;
+    };
 
 }
--- particules/main.js
+++ particules/main.js
@@ -1,83 +1,81 @@
 var main;
 
 window.onload = function () {
-  var sma, vue, env, trace;
-  var agents = [];
-  //make the Math.random predictible and reproducible
-  Math.seedrandom( config.seed || Math.random() + '' );
+    var sma, vue, env, trace;
+    var agents = [];
+    //make the Math.random predictible and reproducible
+    Math.seedrandom(config.seed || Math.random() + '');
 
-  function createAgents( particules ) {
-    var keys = Object.keys( particules );
-    var pos = {
-      x: Math.floor( Math.random() * config.grid.size.x ),
-      y: Math.floor( Math.random() * config.grid.size.y )
-    };
+    function createAgents(particules) {
+        var keys = Object.keys(particules);
+        var pos = {
+            x: Math.floor(Math.random() * config.grid.size.x),
+            y: Math.floor(Math.random() * config.grid.size.y)
+        };
 
-    for ( var i = 0; i < keys.length; i++ ) {
-      for ( j = particules[ keys[ i ] ]; j > 0; j-- ) {
-        while ( !env.isFree( pos ) ) {
-          pos.x = Math.floor( Math.random() * config.grid.size.x );
-          pos.y = Math.floor( Math.random() * config.grid.size.y );
+        for (var i = 0; i < keys.length; i++) {
+            for (j = particules[keys[i]]; j > 0; j--) {
+                while (!env.isFree(pos)) {
+                    pos.x = Math.floor(Math.random() * config.grid.size.x);
+                    pos.y = Math.floor(Math.random() * config.grid.size.y);
+                }
+                agents.push(createAgent(keys[i], pos.x, pos.y, env));
+                env.moveAgent(agents[agents.length - 1], {
+                    x: pos.x,
+                    y: pos.y
+                });
+            }
         }
-        agents.push( createAgent( keys[ i ], pos.x, pos.y, env ) );
-        env.moveAgent( agents[ agents.length - 1 ], {
-          x: x,
-          y: y
-        } );
-      }
+        sma = new SMA(agents, config.refresh);
+        env.setSMA(sma);
+        sma.addObserver(vue);
+        vue.update(agents);
     }
-    sma = new SMA( agents, config.refresh );
-    env.setSMA( sma );
-    sma.addObserver( vue );
-    vue.update( agents );
-  }
 
-  function createTrace() {
-    if ( config.trace ) {
-      trace = new Trace();
-      sma.addObserver( trace );
+    function createTrace() {
+        if (config.trace) {
+            trace = new Trace();
+            sma.addObserver(trace);
+        }
     }
-  }
 
-  env = new Environment( config.grid.size.x, config.grid.size.y, config.grid.toric );
-  vue = createVue( config.render || "TableVue", document.getElementById( 'view' ), env );
+    env = new Environment(config.grid.size.x, config.grid.size.y, config.grid.toric);
+    vue = createVue(config.render || "TableVue", document.getElementById('view'), env);
 
-  createAgents( config.particules );
-  createTrace();
+    createAgents(config.particules);
+    createTrace();
 
-  sma.run();
+    sma.run();
 
-  main = {
-    nextTick: function () {
-      sma.launchTurn();
-    },
-    agents: function () {
-      return agents;
-    },
-    printAgents: function () {
-      var res = "";
-      for ( i = 0; i < agents.length; i++ ) {
-        res = res + "x: " + agents[ i ].x() + " y: " + agents[ i ].y() + "\n";
-      }
-      return res;
-    }
-  };
+    main = {
+        nextTick: function () {
+            sma.launchTurn();
+        },
+        agents: function () {
+            return agents;
+        },
+        printAgents: function () {
+            var res = "";
+            for (i = 0; i < agents.length; i++) {
+                res = res + "x: " + agents[i].x() + " y: " + agents[i].y() + "\n";
+            }
+            return res;
+        }
+    };
 };
-();
 
 main = {
-  nextTick: function () {
-    sma.launchTurn();
-  },
-  agents: function () {
-    return agents;
-  },
-  printAgents: function () {
-    var res = "";
-    for ( i = 0; i < agents.length; i++ ) {
-      res = res + "x: " + agents[ i ].x() + " y: " + agents[ i ].y() + "\n";
+    nextTick: function () {
+        sma.launchTurn();
+    },
+    agents: function () {
+        return agents;
+    },
+    printAgents: function () {
+        var res = "";
+        for (i = 0; i < agents.length; i++) {
+            res = res + "x: " + agents[i].x() + " y: " + agents[i].y() + "\n";
+        }
+        return res;
     }
-    return res;
-  }
-};
 };
