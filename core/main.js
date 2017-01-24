57b4799d420e702a2d3c3bc767fe1d879f60b787
valtena <ethanvaltena@gmail.com>
2017-01-25 00:34:31 +0100

    main as object

--- core/main.js
+++ core/main.js
@@ -1,12 +1,24 @@
-var main = {};
+class Main{
+  constructor(){
+    this.isinit=true;
+  }
 
-function newSimu() {
-  var sma, vue, env, trace;
-  var agents = [];
-  //make the Math.random predictible and reproducible
-  Math.seedrandom( config.seed || Math.random() + '' );
+  createTrace() {
+    if ( config.trace ) {
+      this.trace = new Trace( null, this.env );
+      this.sma.addObserver( this.trace );
+    }
+  }
 
-  function createAgents( particules ) {
+  createPanel() {
+    if ( config.panel ) {
+      this.panel = new PanelVue( document.getElementById( 'panel' ), this.env );
+      this.sma.addObserver( this.panel );
+    }
+  }
+
+  createAgents( particules ) {
+    var agents = [];
     var keys = Object.keys( particules );
     var pos = {
       x: Math.floor( Math.random() * config.grid.size.x ),
@@ -14,70 +26,67 @@ function newSimu() {
     };
 
     for ( var i = 0; i < keys.length; i++ ) {
-      for ( j = particules[ keys[ i ] ]; j > 0; j-- ) {
-        while ( !env.isFree( pos ) ) {
+      for ( var j = particules[ keys[ i ] ]; j > 0; j-- ) {
+        while ( !this.env.isFree( pos ) ) {
           pos.x = Math.floor( Math.random() * config.grid.size.x );
           pos.y = Math.floor( Math.random() * config.grid.size.y );
         }
-        agents.push( createAgent( keys[ i ], pos.x, pos.y, env ) );
-        env.moveAgent( agents[ agents.length - 1 ], {
+        agents.push( createAgent( keys[ i ], pos.x, pos.y, this.env ) );
+        this.env.moveAgent( agents[ agents.length - 1 ], {
           x: pos.x,
           y: pos.y
         } );
       }
     }
+    this.agents=agents
     return agents;
   }
 
-  function createTrace() {
-    if ( config.trace ) {
-      trace = new Trace( null, env );
-      sma.addObserver( trace );
-    }
+  createSimu() {
+    //make the Math.random predictible and reproducible
+    Math.seedrandom( config.seed || Math.random() + '' );
+
+    this.env = new Environment( config.grid.size.x, config.grid.size.y, config.grid.toric );
+    this.vue = createVue( config.render || "TableVue", document.getElementById( 'view' ), this.env );
+
+    this.sma = new SMA(
+      this.createAgents( config.particules ), config.particules );
+    this.env.setSMA( this.sma );
+    this.sma.addObserver( this.vue );
+    this.vue.update( this.agents );
+    this.createTrace();
+    this.createPanel();
+
+    this.sma.run();
+
   }
 
-  function createPanel() {
-    if ( config.panel ) {
-      panel = new PanelVue( document.getElementById( 'panel' ), env );
-      sma.addObserver( panel );
-    }
+  deleteSimu(){
+      this.sma.killAllAgents();
   }
 
-  env = new Environment( config.grid.size.x, config.grid.size.y, config.grid.toric );
-  vue = createVue( config.render || "TableVue", document.getElementById( 'view' ), env );
+  newSimu(){
+    this.deleteSimu();
+    this.createSimu();
+  }
 
-  sma = new SMA(
-    createAgents( config.particules ), config.particules );
-  env.setSMA( sma );
-  sma.addObserver( vue );
-  vue.update( agents );
-  createTrace();
-  createPanel();
-
-  sma.run();
-
-  main = {
-    newSimu: function () {
-      sma.killAllAgents();
-      console.log( "vide" );
-      sma.setNewAgents( createAgents( config.particules ) );
-    },
-    nextTick: function () {
-      sma.launchTurn();
-    },
-    agents: function () {
-      return agents;
-    },
-    printAgents: function () {
-      var res = "";
-      for ( i = 0; i < agents.length; i++ ) {
-        res = res + "x: " + agents[ i ].x() + " y: " + agents[ i ].y() + "\n";
-      }
-      return res;
+  printAgents () {
+    var res = "";
+    for ( i = 0; i < agents.length; i++ ) {
+      res = res + "x: " + agents[ i ].x() + " y: " + agents[ i ].y() + "\n";
     }
-  };
+    return res;
+  }
+
+  nextTick () {
+    this.sma.launchTurn();
+  }
+
+  getAgents () {
+    return this.agents;
+  }
 }
 
-main.newSimu = newSimu;
+var main = new Main();
 
-window.onload = main.newSimu;
+window.onload = function(){main.createSimu()};
